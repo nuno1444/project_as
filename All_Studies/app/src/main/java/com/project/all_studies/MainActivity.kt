82b3a314681f279f5b3c0fa39aaa7e22c9ea5ce4
Seung-Gyu Park <psgi0040@gmail.com>
2021-09-24 21:24:03 +0900

    second
    플로팅 작업 버튼 추가

--- All_Studies/app/src/main/java/com/project/all_studies/MainActivity.kt
+++ All_Studies/app/src/main/java/com/project/all_studies/MainActivity.kt
@@ -7,6 +7,7 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_mypage.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var auth : FirebaseAuth
@@ -15,14 +16,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         auth = Firebase.auth
-
+/*
         btn_logout.setOnClickListener {
             val intent = Intent(this, LoginActivity::class.java)
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
             startActivity(intent)
             auth?.signOut()
-
-        }
+        }*/
 
         with(supportFragmentManager.beginTransaction()){
             val fragment1= Fragment1()
