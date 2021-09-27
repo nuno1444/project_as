16c87b1f87e6b96e7166686af654284fc6cdc195
Seung-Gyu Park <psgi0040@gmail.com>
2021-09-27 23:03:46 +0900

    Mypage_1
    mypage 초기 모델 1

--- All_Studies/app/src/main/java/com/project/all_studies/Fragment3.kt
+++ All_Studies/app/src/main/java/com/project/all_studies/Fragment3.kt
@@ -1,15 +1,32 @@
 package com.project.all_studies
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.fragment_3.*
+import kotlinx.android.synthetic.main.fragment_3.view.*
 
 class Fragment3 : Fragment() {
 
+    private lateinit var auth : FirebaseAuth
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_3, container, false)
+        auth = Firebase.auth
+        val view: View = inflater.inflate(R.layout.fragment_3, container, false)
+
+        view.logout_btn.setOnClickListener {
+            val intent = Intent(activity, LoginActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            startActivity(intent)
+            auth?.signOut()
+        }
+
+        return view
     }
- 
 }
